35a196437ad9d3248b09f0ba773b9e56b0e47f9f
Courtney Kossick <courtney.kossick.05@gmail.com>
2025-05-05 21:58:25 -0400

    Updates to files to make them more efficient, and make the UI more user friendly

---
+++ FitnessFiend/FitnessFiendApp.swift
@@ -0,0 +1,15 @@
+//
+//  FitnessFiendApp.swift
+//  FitnessFiend
+//
+//  Created by Courtney Kossick on 5/5/25.
+//
+
+@main
+struct FitnessFiendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
